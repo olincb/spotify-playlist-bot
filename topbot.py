b46b1d1b6eff9181b09de94bd7484729b29ccd8c
olincb <bailson.jr@gmail.com>
2023-07-07 00:14:55 -0400

    make the callback return the lists of added and removed songs

--- topbot.py
+++ topbot.py
@@ -1,14 +1,12 @@
 from base64 import b64encode
 from dotenv import load_dotenv
 import os
+import pprint
 import requests
 import socket
 import time
 from urllib.parse import (urlencode, parse_qs)
 from uuid import uuid4
-
-normal_print = print
-from rich import print
 
 
 load_dotenv()
@@ -18,60 +16,112 @@ N_TRACKS = 25
 class TopBot:
 
     def __init__(self):
-        self.token = self.authenticate()
-        self.get_users_id_and_name()
-        self.pl_description = f'Auto-generated. Updated {time.strftime("%B %-d, %Y")}.'
+        self.port = 4242
+        self.callback_url = f'http://localhost:{self.port}/callback'
 
-    def authenticate(self, port=4242):
-        client_id = os.getenv('CLIENT_ID')
-        client_secret = os.getenv('CLIENT_SECRET')
-        spotify_credentials = b64encode(f'{client_id}:{client_secret}'.encode('ascii')).decode('ascii')
-        callback = f'http://localhost:{port}/callback'
-        state = str(uuid4())
-        scope = 'user-top-read playlist-modify-public' # user-read-private user-read-email'
+        self.spotify_scope = 'user-top-read playlist-modify-public'
+        self.state = str(uuid4())
+
+        self.spotify_client_id = os.getenv('CLIENT_ID')
+        self.spotify_client_secret = os.getenv('CLIENT_SECRET')
+        if not (self.spotify_client_id and self.spotify_client_secret):
+            raise ValueError('Need Spotify Client ID and Secret.')
+        self.spotify_credentials = b64encode(f'{self.spotify_client_id}:{self.spotify_client_secret}'.encode('ascii')).decode('ascii')
+        self.auth_url = self.form_auth_url()
+
+
+    def form_auth_url(self):
         params = {
             'response_type': 'code',
-            'client_id': client_id,
-            'scope': scope,
-            'redirect_uri': callback,
-            'state': state
+            'client_id': self.spotify_client_id,
+            'scope': self.spotify_scope,
+            'redirect_uri': self.callback_url,
+            'state': self.state
         }
         auth_url = f'https://accounts.spotify.com/authorize?{urlencode(params)}'
-        # Can't get webbrowser to open from within WSL so I'm giving up on that and requiring user to click link
-        normal_print(f'Authenticate here: {auth_url}')
+        return auth_url
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.bind(('localhost', port))
-            sock.listen()
-            csock, caddr = sock.accept()
-            req = csock.recv(1024).decode()
-            res = 'HTTP/1.0 200 OK\n\nAuthentication successful. You may return to your terminal.\n'
-            csock.sendall(res.encode())
-            csock.close()
-        finally:
-            sock.close()
 
+    def parse_code_from_request(self, req):
         endpoint = req.split()[1]
         query_string = endpoint.split('?')[1]
         req_params = parse_qs(query_string)
         recieved_state = req_params['state'][0]
-        if recieved_state != state:
-            print(f'State mismatch.\nGot:      {recieved_state}\nExpected: {state}')
+        if recieved_state != self.state:
+            print(f'State mismatch.\nGot:      {recieved_state}\nExpected: {self.state}')
             return None
         code = req_params['code'][0]
+        return code
 
+
+    def get_token(self, code):
         token_endpoint = 'https://accounts.spotify.com/api/token'
-        post_body = {'code': code, 'redirect_uri': callback, 'grant_type': 'authorization_code'}
-        headers = {'Authorization': f'Basic {spotify_credentials}', 'Content-Type': 'application/x-www-form-urlencoded'}
+        post_body = {'code': code, 'redirect_uri': self.callback_url, 'grant_type': 'authorization_code'}
+        headers = {'Authorization': f'Basic {self.spotify_credentials}', 'Content-Type': 'application/x-www-form-urlencoded'}
         r = requests.post(token_endpoint, data=post_body, headers=headers)
         token = r.json()['access_token']
         return token
 
+
+    def format_return_lists(self, added, removed):
+        """
+        This modifies the arguments but it's fine because we don't need them anymore
+        """
+        for l in [added, removed]:
+            for t in l:
+                del t['id']
+                del t['album_id']
+                del t['uri']
+                t['artists'] = [a['name'] for a in t['artists']]
+
+        pp = pprint.PrettyPrinter(indent=4, width=140)
+        added_str = pp.pformat(added)
+        removed_str = pp.pformat(removed)
+        return added_str, removed_str
+
+
+    def update_top_tracks_playlist(self):
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind(('localhost', self.port))
+            sock.listen()
+
+            print(f'Authenticate here: {self.auth_url}')
+
+            # Should be able to `while True:` the remainder of this.
+            # I'll have to deal with token refreshing though.
+            csock, caddr = sock.accept()
+            req = csock.recv(1024).decode()
+
+            try:
+                code = self.parse_code_from_request(req)
+                self.token = self.get_token(code)
+
+                added, removed = self.do_update()
+
+                if added or removed:
+                    added_str, removed_str = self.format_return_lists(added, removed)
+                    res = '\n'.join([
+                        'HTTP/1.0 200 OK\n',
+                        'Added:',
+                        added_str,
+                        'Removed:',
+                        removed_str,
+                        ''
+                    ])
+                else:
+                    res = 'HTTP/1.0 200 OK\n\nNo changes to playlist.\n'
+                csock.sendall(res.encode())
+            except Exception as e:
+                res = 'HTTP/1.0 500 Internal Server Error\n\nSomething went wrong. Sorry.\n'
+                csock.sendall(res.encode())
+                raise e
+            finally:
+                csock.close()
+
+
     def api_call(self, endpoint, method='get', payload={}):
-        if not self.token:
-            self.authenticate()
 
         headers = {'Authorization': f'Bearer {self.token}'}
         if method == 'get':
@@ -151,8 +201,9 @@ class TopBot:
         return plid
 
     def update_playlist_description(self):
+        pl_description = f'Auto-generated. Updated {time.strftime("%B %-d, %Y")}.'
         api_endpoint = f'https://api.spotify.com/v1/playlists/{self.plid}'
-        body = {'description': self.pl_description}
+        body = {'description': pl_description}
         res = self.api_call(api_endpoint, method='put', payload=body)
         print(res)
 
@@ -179,7 +230,9 @@ class TopBot:
         res = self.api_call(api_endpoint, method='post', payload=payload)
 
 
-    def update_top_tracks_playlist(self, n=N_TRACKS):
+    def do_update(self, n=N_TRACKS):
+        # get user's info
+        self.get_users_id_and_name()
 
         # create playlist if it doesn't exist
         playlists = self.get_playlists()
@@ -202,26 +255,30 @@ class TopBot:
               f'num top tracks: {len(top_tracks)}')
 
         # remove playlist tracks no longer in top n tracks
-        to_remove = [
-            t for t in pl_track_uris
-            if not (t in top_track_uris)
-        ]
+        to_remove = {
+            t['uri']: t
+            for t in pl_tracks
+            if not (t['uri'] in top_track_uris)
+        }
         print(f'removing {len(to_remove)} tracks')
         if to_remove:
-            self.remove_tracks(to_remove)
+            self.remove_tracks(list(to_remove.keys()))
 
         
         # add remaining top tracks to playlist
-        to_add = [
-            t for t in top_track_uris
-            if not (t in pl_track_uris)
-        ]
+        to_add = {
+            t['uri']: t
+            for t in top_tracks
+            if not (t['uri'] in pl_track_uris)
+        }
         print(f'adding {len(to_add)} tracks')
         if to_add:
-            self.add_tracks(to_add)
+            self.add_tracks(list(to_add.keys()))
 
         # update the playlist description
         self.update_playlist_description()
+
+        return list(to_add.values()), list(to_remove.values())
 
 
 
